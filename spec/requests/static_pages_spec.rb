413a2e021f1205f88f71fa6804dc859936f228dc
Aaron Chambers <achambers@gmail.com>
2012-10-30 12:22:49 +1030

    Improved static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,51 +4,45 @@ describe "Static pages" do
   
   subject { page }
   
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+  
   describe "Homepage" do
     
     before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
     
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: '| Home') }
   end
   
   describe "Help page" do
     
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1', text: 'Help')
-    end
-    
-    it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title', text: full_title('Help'))
-    end
+    before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
   
   describe "About page" do
-    
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1', text: 'About Us')
-    end
-    
-    it "should have the title 'About Us'" do
-      visit about_path
-      page.should have_selector('title', text: full_title('About Us'))
-    end
+
+    before { visit about_path }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+
+    it_should_behave_like "all static pages"
   end
   
   describe "Contact page" do
+
+    before { visit contact_path }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
     
-    it "should have the h1 Contact" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
-    end
-    
-    it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', text: full_title('Contact'))
-    end
+    it_should_behave_like "all static pages"
   end
 end
